3285c845fff35d0b66e07ac4952323a6c99074cc
baguspanji <pitungs060@gmail.com>
2022-01-03 17:05:42 +0700

    add firebase

--- app.controller.js
+++ app.controller.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const nodemailer = require('nodemailer');
+const firebase = require('firebase/compat/app')
+require('firebase/compat/firestore')
 
 module.exports = (app) => {
     app.use('/', router);
@@ -40,4 +42,57 @@ router.post('/mail', async (req, res) => {
             message_err: err
         })
     }
+})
+
+var fire = firebase.initializeApp({
+    apiKey: "AIzaSyC3alM6CzmTWBwpIeBQr8jIdrB-vdIq6iU",
+    authDomain: "klambimu-id.firebaseapp.com",
+    databaseURL: "https://klambimu-id-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "klambimu-id",
+    storageBucket: "klambimu-id.appspot.com",
+    messagingSenderId: "886549110355",
+    appId: "1:886549110355:web:bb6e9bec160ec92881f0a6",
+    measurementId: "G-LLB0TZHJR3"
+});
+
+router.post('/dropbox', async (req, res) => {
+    const { current_weight, dropbox_id } = req.body;
+    var db = fire.firestore()
+
+    if (current_weight == null || dropbox_id == null) {
+        res.status(500).json({
+            status: false,
+            message: "current_weight / dropbox_id tidak boleh kosong",
+        })
+    } else {
+        try {
+            db.settings({
+                timestampsInSnapshots: true
+            })
+            db.collection('dropbox_item').add({
+                current_weight,
+                dropbox_id,
+                dropbox: db.collection('dropbox').doc(dropbox_id),
+                createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+                status: 0
+            })
+
+            res.status(200).json({
+                status: true,
+                message: "Data Created",
+                message_id: {
+                    current_weight,
+                    dropbox_id,
+                    createdAt: Date(),
+                }
+            })
+        } catch (err) {
+            res.status(500).json({
+                status: false,
+                message: "Data error Created",
+                message_err: err
+            })
+        }
+    }
+
 })
